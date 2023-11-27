453c8ac478ab56eea2374ffb930f0515b48ee586
Ring <tnam190501@gmail.com>
2023-11-27 17:37:26 +0700

    Code , Plugin
    +Code cho player di chuyển
    +thêm các gói plugins cinemachine , joystick , leanPool.

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+    [SerializeField] Transform _meshPlayer;
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rb.velocity = direction * speed;
+        if (direction != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            _meshPlayer.transform.rotation = Quaternion.RotateTowards(_meshPlayer.transform.rotation, toRotation, 550 * Time.fixedDeltaTime);
+        }
+    }
+}
